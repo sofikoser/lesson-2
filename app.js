0d8d7a24073b0d672c4ad30633de00dab86759c5
sofikoser <ssssofisss@gmail.com>
2025-04-14 17:34:50 +0500

    homework

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+const firstName = "Sofiya";
+let age = 26;
+let city = "Aktau";
+let hobby = "handmade crafts";
+
+console.log(
+  `My name is ${firstName}, I am ${age} years old, I live in ${city}, and my hobby is ${hobby}.`
+);
